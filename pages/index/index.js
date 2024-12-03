8800a498ba0e96d9113cc338b09fbbe6e5ed0398
wangzhenyu2018 <563195949@qq.com>
2024-12-03 13:37:13 +0800

    轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,63 +4,79 @@ Page({
       {
         id: 1,
         name: '电子产品',
-        icon: '/assets/phone.png',
+        icon: '/images/Electronic-Products.png',
       },
       {
         id: 2,
         name: '厨房设备',
-        icon: '/assets/house.png',
+        icon: '/images/Furniture-Items.png',
       },
       {
         id: 3,
-        name: '工业设备',
-        icon: '/assets/camera.png',
+        name: '办公设备',
+        icon: '/images/Office-Equipment.png',
       },
       {
         id: 4,
-        name: '交通工具',
-        icon: '/assets/car.png',
-        desc: '5折'
+        name: '家用电器',
+        icon: '/images/Furniture-Appliances.png',
       },
       {
         id: 5,
         name: '全部',
-        icon: '/assets/all.png',
+        icon: '/images/icon/grid.png',
       }
     ],
+    // 热阻爆款
     promoTags: [
       { id: 1, name: '热租爆款' },
       { id: 2, name: '二手优选' },
       { id: 3, name: '潮玩数码' },
       { id: 4, name: '运动户外' },
-      { id: 5, name: '都市丽人' }
+      { id: 5, name: '餐饮设备' }
     ],
+    // 产品轮播图
+
+    currentIndex: 0,
     products: [
       {
         id: 1,
-        name: 'HUAWEI Mate XT 非凡大师',
-        image: '/assets/huawei.png',
-        price: '48.28',
-        tag: '全新',
-        tags: ['可续租', '租期保障']
+        name: "HUAWEI Mate XT 非凡大屏",
+        price: "¥48.28/天",
+        image: "/images/product/1.jpg"
       },
       {
         id: 2,
-        name: 'iPhone 15',
-        image: '/assets/iphone15.png',
-        price: '0.03',
-        tag: '全新',
-        tags: ['可买断', '可续租']
+        name: "HUAWEI P60 Pro",
+        price: "¥52.99/天",
+        image: "/images/product/2.jpg"
       },
       {
         id: 3,
-        name: '小米 15 Pro',
-        image: '/assets/xiaomi.png',
-        price: '9.87',
-        tag: '全新',
-        tags: ['立减88', '可买断', '可续租']
+        name: "HUAWEI MatePad Pro",
+        price: "¥45.50/天",
+        image: "/images/product/3.jpg"
+      },
+      {
+        id: 4,
+        name: "HUAWEI Watch GT4",
+        price: "¥35.99/天",
+        image: "/images/product/product4.jpg"
+      },
+      {
+        id: 4,
+        name: "HUAWEI Watch GT4",
+        price: "¥35.99/天",
+        image: "/images/product/5.jpg"
       }
     ]
+    
+  },
+  // 页面加载时轮播图的逻辑
+  onSwiperChange(e) {
+    this.setData({
+      currentIndex: e.detail.current
+    });
   },
 
   onLoad() {
@@ -69,27 +85,15 @@ Page({
 
   onSearch() {
     // 搜索功能
-    my.showToast({
-      content: '搜索功能开发中'
-    });
   },
 
   onCategoryTap(event) {
     const { id } = event.target.dataset;
     // 处理分类点击
-    my.showToast({
-      content: `点击了分类${id}`
-    });
-    
   },
-  
 
   onProductTap(event) {
-    const { id } = event.target.dataset;
-    // 处理商品点击
-    my.navigateTo({
-      url: `/pages/product/detail?id=${id}`
-    });
+    // 处理产品点击
   }
 });
 
